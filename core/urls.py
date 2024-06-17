283e1da9ba35e24d688de54676b063174c6c40e1
LordLucass <lucasnoalpottergithub@gmail.com>
2024-06-17 13:12:34 -0300

    feat(logout): Secure logout done #5

--- core/urls.py
+++ core/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("terms/", views.terms, name="terms"),
     path("policy/", views.policy, name="policy"),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path("login/", auth_views.LoginView.as_view(authentication_form=LoginForm), name="login")
 ]
